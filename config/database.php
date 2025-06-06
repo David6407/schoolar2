f51e41302603333520f82a86adcecbac3c88504a
dabi <davidblancopai11.5@gmail.com>
2025-06-05 22:11:58 -0500

    final

--- config/database.php
+++ config/database.php
@@ -1,19 +1,13 @@
 <?php
-//Config connection
+
     $host="aws-0-us-east-1.pooler.supabase.com";
     $port="6543";
     $dbname="postgres";
     $user ="postgres.vohhzuvqvrtdxmqvhaau";
     $password="unicesmag@@";
 
-/*
-    $host="localhost";
-    $port="5432";
-    $dbname="schoolar";
-    $user ="postgres";
-    $password="unicesmag";
-*/
-//Create connection
+
+
     $conn = pg_connect("
         host = $host
         port = $port
